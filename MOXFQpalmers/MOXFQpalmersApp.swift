886d1560dfc1d50c11936f4d6b4938c1985a0ca2
simon palmer <simonhpalmer@icloud.com>
2023-02-06 15:34:40 +0000

    Initial Commit

---
+++ MOXFQpalmers/MOXFQpalmersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOXFQpalmersApp.swift
+//  MOXFQpalmers
+//
+//  Created by simon palmer on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MOXFQpalmersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
